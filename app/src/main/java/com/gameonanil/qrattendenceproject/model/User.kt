2b430180f07133505363e32e302a2bbf15df6d2d
Anil Thapa <67099170+gameonanil@users.noreply.github.com>
2021-09-19 19:14:53 +0545

    Finally added save to excel feature to mainfragment

--- app/src/main/java/com/gameonanil/qrattendenceproject/model/User.kt
+++ app/src/main/java/com/gameonanil/qrattendenceproject/model/User.kt
@@ -8,7 +8,10 @@ import kotlinx.parcelize.Parcelize
  data class User(
     val uid: String? = "",
     val username: String? ="",
+    val roll: String?="",
+    val phone: String?="",
     val email: String? ="",
     val password: String?="",
     val user_type: String?=""
+
 ): Parcelable
--- app/src/main/java/com/gameonanil/qrattendenceproject/ui/teacher/MainTeacherFragment.kt
+++ app/src/main/java/com/gameonanil/qrattendenceproject/ui/teacher/MainTeacherFragment.kt
@@ -1,9 +1,13 @@
 package com.gameonanil.qrattendenceproject.ui.teacher
 
-import android.content.Context
+
+import android.content.ContentValues
 import android.content.DialogInterface
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.*
 import android.widget.Toast
@@ -22,6 +26,18 @@ import com.gameonanil.qrattendenceproject.model.User
 import com.gameonanil.qrattendenceproject.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import org.apache.poi.hssf.usermodel.HSSFCellStyle
+import org.apache.poi.hssf.usermodel.HSSFFont
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.hssf.util.HSSFColor
+import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.CellStyle
+import org.apache.poi.ss.usermodel.IndexedColors
+import org.apache.poi.ss.usermodel.Row
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 
 import java.text.SimpleDateFormat
 import java.util.*
@@ -42,7 +58,10 @@ class MainTeacherFragment : Fragment(), AttendanceAdapter.OnAttendanceClickListe
     private lateinit var attendanceList: MutableList<User>
     private lateinit var teacherId: String
     private lateinit var appBarConfiguration: AppBarConfiguration
-
+    private var cell: Cell? = null
+    private var row: Row? = null
+    private lateinit var defaultStyle: CellStyle
+    private lateinit var currentDate:String
 
 
     override fun onCreateView(
@@ -87,6 +106,7 @@ class MainTeacherFragment : Fragment(), AttendanceAdapter.OnAttendanceClickListe
         val date = Calendar.getInstance().time
         val formatter = SimpleDateFormat("yyyy.MM.dd")
         val formattedDate = formatter.format(date)
+        currentDate = formattedDate
 
 
         val collection = firestore
@@ -118,6 +138,15 @@ class MainTeacherFragment : Fragment(), AttendanceAdapter.OnAttendanceClickListe
                     MainTeacherFragmentDirections.actionMainTeacherFragmentToGeneratorFragment()
                 findNavController().navigate(action)
             }
+
+            buttonDownloadAttendance.setOnClickListener {
+                if(attendanceList.isNotEmpty()){
+                    handleDownloadAttendance()
+                }else{
+                    Toast.makeText(requireContext(), "No Attendance to Download", Toast.LENGTH_SHORT).show()
+                }
+
+            }
         }
 
         return binding.root
@@ -147,6 +176,115 @@ class MainTeacherFragment : Fragment(), AttendanceAdapter.OnAttendanceClickListe
         return super.onOptionsItemSelected(item)
     }
 
+    private fun handleDownloadAttendance(){
+        val wb = HSSFWorkbook()
+        val sheet = wb.createSheet()
+
+        val defaultFont: HSSFFont = wb.createFont()
+        defaultFont.fontHeightInPoints = 10.toShort()
+        defaultFont.fontName = "Arial"
+        defaultFont.color = IndexedColors.BLACK.index
+        defaultFont.boldweight = HSSFFont.BOLDWEIGHT_BOLD
+
+
+        val cellStyle1: CellStyle = wb.createCellStyle()
+        cellStyle1.fillForegroundColor = HSSFColor.AQUA.index
+        cellStyle1.fillPattern = HSSFCellStyle.SOLID_FOREGROUND
+        cellStyle1.alignment = CellStyle.ALIGN_CENTER
+        cellStyle1.wrapText
+        cellStyle1.setFont(defaultFont)
+
+        val cellStyle2: CellStyle = wb.createCellStyle()
+        cellStyle2.alignment = CellStyle.ALIGN_CENTER
+        cellStyle2.wrapText
+
+        row = sheet.createRow(0)
+        defaultStyle = cellStyle1
+        cell = row!!.createCell(0);
+        cell?.setCellValue("Roll")
+        cell?.cellStyle = defaultStyle
+
+        cell = row!!.createCell(1);
+        cell?.setCellValue("Student Name")
+        cell?.cellStyle = defaultStyle
+
+        cell = row!!.createCell(2);
+        cell?.setCellValue("Phone")
+        cell?.cellStyle = defaultStyle
+
+        cell = row!!.createCell(3);
+        cell?.setCellValue("Email")
+        cell?.cellStyle = defaultStyle
+
+        defaultStyle = cellStyle2
+        for (i in attendanceList.indices) {
+            row = sheet.createRow(i+1)
+
+            cell = row!!.createCell(0);
+            cell?.setCellValue(attendanceList[i].roll)
+            cell?.cellStyle = defaultStyle
+
+            cell = row!!.createCell(1);
+            cell?.setCellValue(attendanceList[i].username)
+            cell?.cellStyle = defaultStyle
+
+            cell = row!!.createCell(2);
+            cell?.setCellValue(attendanceList[i].phone)
+            cell?.cellStyle = defaultStyle
+
+            cell = row!!.createCell(3);
+            cell?.setCellValue(attendanceList[i].email)
+            cell?.cellStyle = defaultStyle
+
+
+        }
+
+        sheet.setColumnWidth(0, 3600)
+        sheet.setColumnWidth(1, 5500)
+        sheet.setColumnWidth(2, 3600)
+        sheet.setColumnWidth(3, 8500)
+
+
+        val fos: OutputStream
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                Log.d(TAG, "createExcel: new way called")
+                val resolver = requireActivity().applicationContext.contentResolver
+                val contentValues = ContentValues()
+                contentValues.put(MediaStore.Files.FileColumns.DISPLAY_NAME, "Attendance_$currentDate.xls")
+                contentValues.put(
+                    MediaStore.Files.FileColumns.MIME_TYPE,
+                    "application/vnd.ms-excel"
+                )
+                contentValues.put(
+                    MediaStore.MediaColumns.RELATIVE_PATH,
+                    Environment.DIRECTORY_DOCUMENTS + File.separator + "Attendance"
+                )
+                val excelUri = resolver.insert(
+                    MediaStore.Files.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY),
+                    contentValues
+                )
+                fos = resolver.openOutputStream(Objects.requireNonNull(excelUri)!!)!!
+                wb.write(fos)
+                Toast.makeText(requireContext(), "EXCEL Saved (New way)", Toast.LENGTH_SHORT).show()
+            } else {
+
+                val file = File(
+                    requireActivity().applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
+                    "Attendance_$currentDate.xls"
+                )
+                val outputStream = FileOutputStream(file)
+                wb.write(outputStream)
+
+                Log.d(TAG, "createExcel: Old way called")
+                Toast.makeText(requireContext(), "File stored (Old way)", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            Toast.makeText(requireContext(), "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+        }catch (io: IOException){
+            Toast.makeText(requireContext(), "Error:${io.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
